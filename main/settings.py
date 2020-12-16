f63a3bb5fc1404eafceb1ed419ff4a456ad74f43
Harshit Khanna <65344995+harshit-dot@users.noreply.github.com>
2020-12-16 12:12:52 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -112,27 +112,10 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 
 
-# Database
-# https://docs.djangoproject.com/en/3.1/ref/settings/#databases
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE':'django.db.database.sqlite3',
-#         'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
-       
-#     }
-    
-# }
 DATABASES = {
     'default': dj_database_url.config()
     
 }
-# import dj_database_url
-# db_from_env=dj_database_url.config(conn_max_age=600)
-# DATABASES['default'].update(db_from_env)
-
-# Password validation
-# https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
 
 AUTH_PASSWORD_VALIDATORS = [
     {
