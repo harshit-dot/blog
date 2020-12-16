90900302b643549a08f7861d9d046bcff2aa0c6e
Harshit Khanna <65344995+harshit-dot@users.noreply.github.com>
2020-12-16 12:19:28 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -110,7 +110,7 @@ TEMPLATES = [
 ]
 
 WSGI_APPLICATION = 'main.wsgi.application'
-
+import dj_database_url
 
 db_from_env = dj_database_url.config(conn_max_age=600)
 DATABASES['default'].update(db_from_env)
