60bf5d1377c213e2bb15457156d03125c1754edc
Harshit Khanna <65344995+harshit-dot@users.noreply.github.com>
2020-12-16 12:32:19 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -112,8 +112,9 @@ TEMPLATES = [
 WSGI_APPLICATION = 'main.wsgi.application'
 import dj_database_url
 
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+DATABASES=[
+    'default':dj_database_url.config(),
+]
 
 AUTH_PASSWORD_VALIDATORS = [
     {
