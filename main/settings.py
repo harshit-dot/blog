f1c695e6184d9e543f4bef7e106d344ffc50e576
Harshit Khanna <65344995+harshit-dot@users.noreply.github.com>
2020-12-17 12:13:29 +0530

    Update settings.py

--- main/settings.py
+++ main/settings.py
@@ -128,8 +128,7 @@ DATABASES={
       
    }
 }
-db_from_env = dj_database_url.config(conn_max_age=600)
-DATABASES['default'].update(db_from_env)
+
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
